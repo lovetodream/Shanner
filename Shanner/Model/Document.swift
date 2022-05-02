c62e91dbd953dcf4c885ba5579ec91a69f1e3690
Timo <38291523+lovetodream@users.noreply.github.com>
2022-05-02 21:56:30 +0200

    feat: add helper function to create a pdf to the document along with a convenience initialiser

---
+++ Shanner/Model/Document.swift
@@ -0,0 +1,47 @@
+//
+//  Document.swift
+//  Shanner
+//
+//  Created by Timo Zacherl on 29.04.22.
+//
+
+import Foundation
+import VisionKit
+import PDFKit
+import CoreData
+
+extension Document {
+
+    /// Creates a pdf document from a scan.
+    /// **Be aware that this method does not safe the document to Core Data.**
+    ///
+    /// - Parameter scan: the scan which should be converted to a pdf
+    /// - Returns: the created document
+    static func createPDF(from scan: VNDocumentCameraScan) -> PDFDocument {
+        let pdfDocument = PDFDocument()
+        for pageIndex in 0..<scan.pageCount {
+            if let pdfPage = PDFPage(image: scan.imageOfPage(at: pageIndex)) {
+                pdfDocument.insert(pdfPage, at: pageIndex)
+            }
+        }
+        return pdfDocument
+    }
+
+    /// Initialises a document from a scan and inserts it into the given context.
+    ///
+    /// - Parameters:
+    ///   - context: The context into which the document should be inserted.
+    ///   - scan: The VNDocumentCameraScan used to create the document.
+    convenience init(context: NSManagedObjectContext, scan: VNDocumentCameraScan) {
+        self.init(context: context)
+        let pdf = Self.createPDF(from: scan)
+        self.document = pdf.dataRepresentation()
+        self.pageCount = Int16(pdf.pageCount)
+        self.createdAt = .now
+
+        guard let createdAt = self.createdAt else { return }
+
+        self.title = "Scan \(createdAt.formatted(date: .abbreviated, time: .shortened))"
+    }
+
+}
