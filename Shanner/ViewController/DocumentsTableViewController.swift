1c9798024a323e74f0d0cae347133e565dd85b51
Timo <38291523+lovetodream@users.noreply.github.com>
2022-05-07 11:25:02 +0200

    docs: add docs for DocumentsTableViewController

--- Shanner/ViewController/DocumentsTableViewController.swift
+++ Shanner/ViewController/DocumentsTableViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import CoreData
 
+/// The table view controller, responsible to show all the documents stored in core data.
 class DocumentsTableViewController: UITableViewController {
 
     var documents = [Document]()
@@ -16,25 +17,25 @@ class DocumentsTableViewController: UITableViewController {
 
     var searchController = UISearchController(searchResultsController: nil)
 
+    /// Gets the managedObjectContext from ``AppDelegate``.
+    /// - Throws: If ``AppDelegate`` does not exist, a fatal error will be thrown.
     lazy var managedObjectContext: NSManagedObjectContext = {
         guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { fatalError("AppDelegate missing") }
         return appDelegate.persistentContainer.viewContext
     }()
 
+    /// Indicates if the user filters the documents at the moment.
     var isFiltering: Bool {
       return searchController.isActive && !(searchController.searchBar.text?.isEmpty ?? true)
     }
 
+    /// Loads the view, adds an edit button, sort button  and ``searchController`` to the navigation item.
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.register(UINib(nibName: DocumentsTableViewCell.reuseIdentifier, bundle: nil),
                            forCellReuseIdentifier: DocumentsTableViewCell.reuseIdentifier)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         self.navigationItem.rightBarButtonItem = self.editButtonItem
 
         searchController.searchResultsUpdater = self
@@ -48,6 +49,8 @@ class DocumentsTableViewController: UITableViewController {
                                                            action: #selector(reverseDocuments(_:)))
     }
 
+    /// Fetches the documents and stores them in ``documents``, once done it reloads the table view.
+    /// - Parameter animated: Indicates if the appearance should be animated or not.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
@@ -65,14 +68,27 @@ class DocumentsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    /// Sets the number of sections to one.
+    /// - Parameter tableView: The table view.
+    /// - Returns: The number of sections, which is one.
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
+    /// Sets the number of rows to the amount of ``documents`` or ``filteredDocuments``, based on ``isFiltering``.
+    /// - Parameters:
+    ///   - tableView: The table view.
+    ///   - section: The index of the current section.
+    /// - Returns: The amount of documents to show.
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return isFiltering ? filteredDocuments.count : documents.count
     }
 
+    /// Sets the cell and its content for a certain row.
+    /// - Parameters:
+    ///   - tableView: The table view.
+    ///   - indexPath: The index path, (section and row).
+    /// - Returns: The configured ``DocumentsTableViewCell``.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: DocumentsTableViewCell.reuseIdentifier,
                                                        for: indexPath) as? DocumentsTableViewCell else {
@@ -86,20 +102,20 @@ class DocumentsTableViewController: UITableViewController {
         return cell
     }
 
+    /// Sets the selected document and performs a segue on row selection.
+    /// - Parameters:
+    ///   - tableView: The table view.
+    ///   - indexPath: The index path, (section and row).
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         selectedDocument = currentDocument(for: indexPath)
         performSegue(withIdentifier: "showDocument", sender: self)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    // Override to support editing the table view.
+    /// Deletes the document at the indexPath, if the editing style is delete, otherwise nothing happens.
+    /// - Parameters:
+    ///   - tableView: The table view.
+    ///   - editingStyle: The editing style, committed by the user.
+    ///   - indexPath: The index path.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
@@ -112,24 +128,12 @@ class DocumentsTableViewController: UITableViewController {
         }
     }
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    /// Does preparation before the segue "showDocument" with ``DocumentViewController`` as its destination, otherwise nothing happens.
+    /// - Parameters:
+    ///   - segue: The segue, containing it's identifier and destination.
+    ///   - sender: The sender, which triggered the segue.
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
@@ -143,6 +147,8 @@ class DocumentsTableViewController: UITableViewController {
         destinationViewController.managedObjectContext = managedObjectContext
     }
 
+    /// Reverses ``documents`` and if applicable ``filteredDocuments``.
+    /// - Parameter target: The action's target.
     @objc func reverseDocuments(_ target: Any) {
         documents.reverse()
         if (isFiltering) { filteredDocuments.reverse() }
@@ -153,6 +159,9 @@ class DocumentsTableViewController: UITableViewController {
 
 extension DocumentsTableViewController: UISearchResultsUpdating {
 
+    /// Updates the search results by filtering the documents and reloads the table view.
+    /// The filtering happens based on the search text being part of the document title.
+    /// - Parameter searchController: The affected search controller.
     func updateSearchResults(for searchController: UISearchController) {
         guard let searchText = searchController.searchBar.text else { return }
         filteredDocuments = documents.filter { document in
@@ -162,6 +171,9 @@ extension DocumentsTableViewController: UISearchResultsUpdating {
         tableView.reloadData()
     }
 
+    /// Gets the current document for the provided index path, based on ``isFiltering``.
+    /// - Parameter indexPath: The index path  to use.
+    /// - Returns: A document.
     func currentDocument(for indexPath: IndexPath) -> Document {
         isFiltering ? filteredDocuments[indexPath.row] : documents[indexPath.row]
     }
