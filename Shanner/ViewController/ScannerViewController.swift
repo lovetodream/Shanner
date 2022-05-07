4a3a5ee431d2e9b237a723a0b11d7e3e17ae9c32
Timo <38291523+lovetodream@users.noreply.github.com>
2022-05-07 11:49:46 +0200

    docs: add docs for ScannerViewController

--- Shanner/ViewController/ScannerViewController.swift
+++ Shanner/ViewController/ScannerViewController.swift
@@ -7,16 +7,20 @@
 
 import VisionKit
 
+/// The view controller to show the document scanner.
 class ScannerViewController: UIViewController {
 
+    /// Gets the managedObjectContext from ``AppDelegate``.
     lazy var managedObjectContext = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
 
+    /// Creates a document camera view controller, sets its delegate and presents it.
+    /// - Parameter animated: Indicates if the appearance should be animated or not.
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
         let documentCameraViewController = VNDocumentCameraViewController()
         documentCameraViewController.delegate = self
-        present(documentCameraViewController, animated: true)
+        present(documentCameraViewController, animated: animated)
     }
 
 }
@@ -24,6 +28,10 @@ class ScannerViewController: UIViewController {
 // MARK: - Document Camera View Controller Delegate
 
 extension ScannerViewController: VNDocumentCameraViewControllerDelegate {
+    /// Called if data was scanned successfully. It saves a new ``Document`` to core data and calls ``dismissToDocuments(animated:)`` when done.
+    /// - Parameters:
+    ///   - controller: The associated view controller.
+    ///   - scan: The document scanned by the user.
     func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
         guard let managedObjectContext = managedObjectContext else {
             dismissToDocuments()
@@ -35,10 +43,16 @@ extension ScannerViewController: VNDocumentCameraViewControllerDelegate {
         dismissToDocuments()
     }
 
+    /// Called if the user cancelled scanning. It calls ``dismissToDocuments(animated:)``.
+    /// - Parameter controller: The associated view controller.
     func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
         dismissToDocuments()
     }
 
+    /// Called if an error happened while scanning. It calls ``dismissToDocuments(animated:)``.
+    /// - Parameters:
+    ///   - controller: The associated view controller.
+    ///   - error: The error which occurred.
     func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFailWithError error: Error) {
         dismissToDocuments()
     }
