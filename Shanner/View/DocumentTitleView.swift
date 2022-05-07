1de1de6bfaf977edc8e0ba0df42d3f644f794ead
Timo <38291523+lovetodream@users.noreply.github.com>
2022-05-07 10:48:19 +0200

    docs: add docs for DocumentTitleView

--- Shanner/View/DocumentTitleView.swift
+++ Shanner/View/DocumentTitleView.swift
@@ -7,12 +7,16 @@
 
 import UIKit
 
+/// The title view for ``DocumentViewController``.
 class DocumentTitleView: UIView {
+
+    /// The label contains the "real" title.
     lazy var labelView: UILabel = {
         var titleView = UILabel()
         return titleView
     }()
 
+    /// The edit button with a pencil image. It contains no action by default.
     lazy var buttonView: UIButton = {
         let image = UIImage(systemName: "pencil")
         var buttonConfiguration = UIButton.Configuration.borderless()
@@ -21,6 +25,9 @@ class DocumentTitleView: UIView {
         return UIButton(configuration: buttonConfiguration)
     }()
 
+    /// Initialises the view with a title.
+    /// It adds the required views and constraints to the view and sets the text for ``labelView``.
+    /// - Parameter title: The title to use.
     init(title: String?) {
         super.init(frame: .zero)
         labelView.text = title
